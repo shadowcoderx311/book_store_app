0a0490d9d140d5e24dba4e1316c29916b44890ad
shadowcoderx316 <redx318@gmail.com>
2016-05-02 17:07:40 +0000

    commit files

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -2,26 +2,34 @@ require 'rails_helper'
 
 RSpec.describe AuthorsController, :type => :controller do
     describe "GET #index" do
-      it "returns a successful http request status code" do
-        get :index
-        
-        expect(response).to have_http_status(:success)
-      end
-    end
     
     describe "GET #show" do
-      it "returns a successful http request status code" do
-        author = Fabricate(:author)
-        
-        get :show, id: author.id
-        expect(response).to have_http_status(:success)
-      end
-    end
     
-    describe "GET #new" do
-      it "returns a successful http request status code" do
-        get :new
-        expect(response).to have_http_status(:success)
+    describe "POST #new" do
+        context "a successful create" do
+          it "saves the new author object" do
+            post :create, author: Fabricate.attributes_for(:author)
+            
+            expect(Author.count).to eq(1)
+          end
+        end
+        
+        it "redirects to the author show action" do
+          post :create, author: Fabricate.attributes_for(:author)
+          
+          expect(response).to redirect_to author_path(Author.last)
+        end
       end
-    end
+        
+        it "sets the success flash message" do
+          post :create, author: Fabricate.attributes_for(:author)
+          
+          expect(flash[:success]).to eq("Author has been created")
+        end
+      end
+        
+        context "an unsuccessful create" do
+          
+   end
+  end
 end
