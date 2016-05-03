e2a2c8caa829adea66fb37ff32b9ea41df3e2a41
shadowcoderx316 <redx318@gmail.com>
2016-05-03 17:30:49 +0000

    commit L50 files

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ end
 group :test do
   gem 'capybara', '2.4.4'
   gem 'faker'
+  gem 'shoulda-matchers', require: false
 end
 
 group :test do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,20 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require "bundler/setup"
+
+::Bundler.require(:default, :test)
+
+require "shoulda/matchers"
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
