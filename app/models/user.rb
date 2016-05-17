f832eaf138a3080f9c7c6cab38e39b14dce4fa6d
shadowcoderx316 <redx318@gmail.com>
2016-05-17 15:53:10 +0000

    commit L73 files

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
   
   validates :first_name, :last_name, :email, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+      format: { with: /\A[A-Z0-9#_-~!$&'()*+,;=:.%]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i}
   
   def full_name
     "#{first_name} #{last_name}"
