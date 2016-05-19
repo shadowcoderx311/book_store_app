1ef07f22429e6d0f229489005c694f58c9890234
shadowcoderx316 <redx318@gmail.com>
2016-05-19 16:24:02 +0000

    commit L82 files

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -5,4 +5,9 @@ Fabricator(:user) do
   email { Faker::Internet.email }
   password 'password'
   password_confirmation 'password'
+  admin false
+end
+
+Fabricator(:admin, from: :user) do
+  admin true
 end
