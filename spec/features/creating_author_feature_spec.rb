90186e17f47df42323dcd1f3f210e70aa388a3be
shadowcoderx316 <redx318@gmail.com>
2016-05-24 20:23:54 +0000

    commit files

--- spec/features/creating_author_feature_spec.rb
+++ spec/features/creating_author_feature_spec.rb
@@ -1,32 +1,35 @@
-require 'spec_helper'
 require 'rails_helper'
 
-RSpec.feature 'Creating Authors' do
-    let(:admin) { Fabricate(:admin) }
-    
-    before { sign_in_as admin }
-    
-  scenario 'create a valid author' do
+RSpec.feature "Creating Authors" do
+  let(:admin) { Fabricate(:admin) }
+
+  before { sign_in_as admin }
+
+  scenario "with valid inputs succeeds" do
     visit root_path
-    click_link 'Authors'
-    click_link 'Add New author'
 
-    fill_in 'First name', with: 'John'
-    fill_in 'Last name', with: 'Bull'
-    click_button 'Create Author'
+    click_link "Authors"
+    click_link "Add New author"
 
-    expect(page).to have_content('Author has been created.')
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Doe"
+
+    click_button "Create Author"
+
+    expect(page).to have_content("Author has been created")
   end
 
-  scenario 'create a invalid author' do
+  scenario "with an invalid input fails" do
     visit root_path
-    click_link 'Authors'
-    click_link 'Add New author'
 
-    fill_in 'First name', with: ''
-    fill_in 'Last name', with: 'Bull'
-    click_button 'Create Author'
+    click_link "Authors"
+    click_link "Add New author"
 
-    expect(page).to have_content('Author has not been created.')
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+
+    click_button "Create Author"
+
+    expect(page).to have_content("Author has not been created")
   end
 end
