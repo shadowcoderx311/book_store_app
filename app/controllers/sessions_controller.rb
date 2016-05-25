48069d863621e4fda05fb961618c618e64fe0f82
shadowcoderx316 <redx318@gmail.com>
2016-05-25 18:12:58 +0000

    commit L87 files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,19 @@
 class SessionsController < ApplicationController
   def new
       redirect_to root_path if current_user
+      #require 'pry';binding.pry
   end
   
   def create
    if user = User.authenticate(params[:email], params[:password])
      session[:user_id] = user.id
      flash[:success] = "Sign in successful"
-     redirect_to root_path
+     if session[:intented_destination]
+       redirect_to session[:intented_destination]
+     else
+       redirect_to root_path
+       session[:intented_destination] = nil
+     end
    else
      flash.now[:danger] = "Invalid email/password combination"
      render :new
