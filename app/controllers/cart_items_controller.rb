ad87c7f82428f215dd76804e1d64e0c3aa8b8a9d
shadowcoderx316 <redx318@gmail.com>
2016-05-27 18:37:49 +0000

    commit L96 and L97 files

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -2,5 +2,14 @@ class CartItemsController < ApplicationController
   before_action :set_cart
   
   def create
+    @cart_item = @cart.add_book_to_items(params[:book_id])
+    respond_to do |format|
+      if @cart_item
+        format.html { redirect_to @cart_item.cart }
+        format.js
+      else
+        format.html { redirect_to catalogs_path }
+      end
+    end
   end
 end
