f55e9be0a91188308db548f244a590dd35bcbd00
shadowcoderx316 <redx318@gmail.com>
2016-05-31 16:39:17 +0000

    commit L101 files

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -1,13 +1,20 @@
 class CatalogsController < ApplicationController
-    def index
-      @books = Book.all
-    end
+  before_action :set_cart_if_session, except: [:search]
+  def index
+    @books = Book.all
+  end
     
-    def show
-      @book = Book.find params[:id]
-    end
+  def show
+    @book = Book.find params[:id]
+    set_cart if session[:cart_id]
+  end
     
-    def search
-      @books = Book.search_by_title(params[:search_word])
-    end
+  def search
+    @books = Book.search_by_title(params[:search_word])
+  end
+    
+  private
+  def set_cart_if_session
+      
+  end
 end
